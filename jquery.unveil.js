5af973bf91bb0f50bbbbbf21d968805738152a2d
Dan BROOKS <githubcom@domaindesign.ca>
2013-10-11 11:44:11 -0700

    Add scrollBox argument to support scroll bars other than window

--- jquery.unveil.js
+++ jquery.unveil.js
@@ -1,3 +1,4 @@
+/*jslint browser: true, sloppy: true, white: true */
 /**
  * jQuery Unveil
  * A very lightweight jQuery plugin to lazy load images
@@ -10,34 +11,37 @@
 
 ;(function($) {
 
-  $.fn.unveil = function(threshold, callback) {
+  $.fn.unveil = function(threshold, callback, options) {
 
     var $w = $(window),
         th = threshold || 0,
         retina = window.devicePixelRatio > 1,
         attrib = retina? "data-src-retina" : "data-src",
         images = this,
-        loaded;
+        loaded,
+        $scrollBox = (options.scrollBox && options.scrollBox.jquery && options.scrollBox.length > 0) ? options.scrollBox : $w;
 
     this.one("unveil", function() {
       var source = this.getAttribute(attrib);
       source = source || this.getAttribute("data-src");
       if (source) {
         this.setAttribute("src", source);
-        if (typeof callback === "function") callback.call(this);
+        if (typeof callback === "function") {
+          callback.call(this);
+        }
       }
     });
 
     function unveil() {
       var inview = images.filter(function() {
-        var $e = $(this);
-        if ($e.is(":hidden")) return;
-
-        var wt = $w.scrollTop(),
+        var $e = $(this),
+            wt = $w.scrollTop(),
             wb = wt + $w.height(),
             et = $e.offset().top,
             eb = et + $e.height();
-
+        if ($e.is(":hidden")) {
+          return;
+        }
         return eb >= wt - th && et <= wb + th;
       });
 
@@ -45,7 +49,7 @@
       images = images.not(loaded);
     }
 
-    $w.scroll(unveil);
+    $scrollBox.scroll(unveil);
     $w.resize(unveil);
 
     unveil();
