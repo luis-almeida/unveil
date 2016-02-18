0cbc6775502e86ca089fbbd2b5573371586fb3d8
Paul Phillips <paulhphil@hotmail.com>
2016-02-18 13:42:37 +0000

    Test if unveiling img tag or changing background-image
    Changes data-src to src for IMG.
    Changes data-src to background-image:url()

--- jquery.unveil.js
+++ jquery.unveil.js
@@ -23,8 +23,12 @@
       var source = this.getAttribute(attrib);
       source = source || this.getAttribute("data-src");
       if (source) {
-        this.setAttribute("src", source);
-        if (typeof callback === "function") callback.call(this);
+        if (this.tagName === 'IMG') {
+          this.setAttribute("src", source);
+        } else {
+          this.style.backgroundImage  = 'url('+source+')';
+        }
+        if (typeof callback === "function") { callback.call(this); }
       }
     });
 
