addd0d490d4783949b72aee0e58e3611f43132bc
Enrico Sottile <EnricoSottile@users.noreply.github.com>
2016-11-25 12:01:26 +0100

    Also load css backgrounds.

--- jquery.unveil.js
+++ jquery.unveil.js
@@ -23,7 +23,14 @@
       var source = this.getAttribute(attrib);
       source = source || this.getAttribute("data-src");
       if (source) {
-        this.setAttribute("src", source);
+        
+          // lazy load css backgrounds aswell
+          $(this).is('img') ? 
+            this.setAttribute("src", source) 
+            :
+            $(this).css("background-image", "url('" + source + "')");
+        
+
         if (typeof callback === "function") callback.call(this);
       }
     });
